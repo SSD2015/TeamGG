adc6b76b45aa4c03e25c51d7e44ca4f5e32b8d8b
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2015-03-07 01:05:15 +0700

    Fixed schema to be compatible with unit testing

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,4 +1,4 @@
 # --- !Ups
-ALTER TABLE `exceedvote`.`user` ADD UNIQUE `username` (`username`);
+ALTER TABLE `user` ADD UNIQUE `username` (`username`);
 # --- !Downs
 ALTER TABLE user DROP INDEX username;
