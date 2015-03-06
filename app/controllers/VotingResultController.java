8c89c0b66d4917c4a5f9ffa67c92d52aa2f931af
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2015-03-06 10:33:57 +0700

    vote_result: Fixed NPE bug, added acl check

--- app/controllers/VotingResultController.java
+++ app/controllers/VotingResultController.java
@@ -4,11 +4,16 @@ import models.Project;
 import models.Vote;
 import models.VoteCategory;
 import play.mvc.Result;
+import utils.Auth;
 
+import static play.mvc.Results.forbidden;
 import static play.mvc.Results.ok;
 
 public class VotingResultController {
     public static Result result(){
+        if(!Auth.acl(Auth.ACL_TYPE.VOTE_RESULT)){
+            return forbidden();
+        }
         return ok(views.html.vote_result.render(
                 Project.find.orderBy("group.number").findList(),
                 VoteCategory.find.all(),
